6dc074d4f196532e6b504ab8a0c2700dbc0ec685
Rey <rey-chyn@qq.com>
2024-04-14 22:54:06 -0400

    update

--- util/util.py
+++ util/util.py
@@ -6,7 +6,7 @@ def build_fewshot_example(question:str, solution:str, userInput: str, correct,mi
     d = {"Correct": correct, "Minor_Mistake": minor_mistake, "Conceptual_Mistake": cm, "Logical_Mistake":lm,"Reasoning": reason}
     return result + str(d)
 
-def build_judge_example(question:str, solution:str, userInput: str, if_answer, incomplete, complete, input_type):
-    result =  "Question: " + question + "\n Solution: " + solution + "\n User Input: " + userInput + "\n \n "
+def build_judge_example(question:str, userInput: str, if_answer, incomplete, complete, input_type):
+    result =  "Question: " + question + "\n User Input: " + userInput + "\n \n "
     d = {"if_answer": if_answer, "incomplete": incomplete, "complete": complete, "input_type": input_type}
     return result + str(d)
